62a33587128a42735c0f40e4b8259a94acf484cf
Aditya Varun Chadha <adichad@gmail.com>
2013-08-08 10:20:58 +0530

    minor changes

--- src/hql/createTable_ETL_HEX_ASSIGNMENT_HIT.hql
+++ src/hql/createTable_ETL_HEX_ASSIGNMENT_HIT.hql
@@ -1,2 +1,19 @@
-create table ETL_HCOM_HEX_ASSIGNMENT_HIT
-(guid string, cid int, experiment_variant_code string, local_date string, gmt int, hit_data_id bigint, new_visitor_ind smallint, page_assigned_entry_page_name string, site_sectn_name string, user_cntext_name string, browser_height smallint, browser_width smallint, brwsr_id smallint, mobile_ind string, destination_id int, property_destination_id int) STORED AS SEQUENCEFILE;
+create table if not exists ETL_HCOM_HEX_ASSIGNMENT_HIT
+(guid string,
+ experiment_variant_code string,
+ local_date string,
+ gmt int,
+ hit_data_id bigint,
+ new_visitor_ind smallint,
+ page_assigned_entry_page_name string,
+ site_sectn_name string,
+ user_cntext_name string,
+ browser_height smallint,
+ browser_width smallint,
+ brwsr_id smallint,
+ mobile_ind string,
+ destination_id int,
+ property_destination_id int)
+partitioned by (cid_bucketed int)
+clustered by (experiment_variant_code, guid) into 512 buckets
+stored as SEQUENCEFILE;
--- src/main/java/udaf/GenericUDAFFirstValueNValueSort.java
+++ src/main/java/udaf/GenericUDAFFirstValueNValueSort.java
@@ -24,177 +24,164 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.hive.serde2.typeinfo.PrimitiveTypeInfo;
 
 /**
- * This is a UDAF that returns the first column value appearing in the sorted rows of a group. 
- * The rows are sorted by the column values provided along. Any no. of sort column values can be provided. 
+ * This is a UDAF that returns the first column value appearing in the sorted
+ * rows of a group. The rows are sorted by the column values provided along. Any
+ * no. of sort column values can be provided.
  * 
- * Input values:
- * 1. Column value to be returned
- * 2. Variable no. of sort column values
+ * Input values: 1. Column value to be returned 2. Variable no. of sort column
+ * values
  * 
- * Output value:
- * Returns column value for the 1st row in the sorted data rows.
+ * Output value: Returns column value for the 1st row in the sorted data rows.
  * 
  */
 public class GenericUDAFFirstValueNValueSort extends AbstractGenericUDAFResolver {
-	private static final Log LOG = LogFactory.getLog(GenericUDAFFirstValueNValueSort.class.getName());
+    private static final Log LOG = LogFactory.getLog(GenericUDAFFirstValueNValueSort.class.getName());
 
-	@Override
-	public GenericUDAFEvaluator getEvaluator(TypeInfo[] parameters)
-			throws SemanticException {
-		if (parameters.length < 2) {
-			throw new UDFArgumentTypeException(
-					2,
-					"Min 2 arguments expected: 1). colm value to be returned, "
-							+ " 2. 1 or more orderBy colms");
-		}
-		// Only Primitive data types are allowed as sort columns
-		for(int num=1; num< parameters.length; num++) {
-			if (parameters[num].getCategory() != ObjectInspector.Category.PRIMITIVE) {
-		      throw new UDFArgumentTypeException(num,
-		          "Only primitive type arguments are accepted in orderby colms but "
-		          + parameters[num].getTypeName() + " was passed as parameter " + num);
-		    }
-			switch (((PrimitiveTypeInfo) parameters[num]).getPrimitiveCategory()) {
-				case VOID:
-				case UNKNOWN: 
-					throw new UDFArgumentTypeException(0,
-							((PrimitiveTypeInfo) parameters[num]).getPrimitiveCategory() + " arguments not acceptable in order by columns");
-				default:
-					break;
-			}
-		}
-		return createEvaluator();
-	}
+    @Override
+    public GenericUDAFEvaluator getEvaluator(TypeInfo[] parameters) throws SemanticException {
+        if (parameters.length < 2) {
+            throw new UDFArgumentTypeException(2, "Min 2 arguments expected: 1). colm value to be returned, " + " 2. 1 or more orderBy colms");
+        }
+        // Only Primitive data types are allowed as sort columns
+        for (int num = 1; num < parameters.length; num++) {
+            if (parameters[num].getCategory() != ObjectInspector.Category.PRIMITIVE) {
+                throw new UDFArgumentTypeException(num, "Only primitive type arguments are accepted in orderby colms but " + parameters[num].getTypeName()
+                        + " was passed as parameter " + num);
+            }
+            switch (((PrimitiveTypeInfo) parameters[num]).getPrimitiveCategory()) {
+            case VOID:
+            case UNKNOWN:
+                throw new UDFArgumentTypeException(0, ((PrimitiveTypeInfo) parameters[num]).getPrimitiveCategory() + " arguments not acceptable in order by columns");
+            default:
+                break;
+            }
+        }
+        return createEvaluator();
+    }
 
-	protected GenericUDAFEvaluator createEvaluator() {
-		return new GenericUDAFOrderedNValueEvaluator();
-	}
-	
-	/*
-	 * Keeps track of the Top key-value pair in the group 
-	 */
-	private static class TopOrderedSet implements AggregationBuffer {
-		private Text sortKey;
-		private Object value;
-		
-		TopOrderedSet() {
-			init();
-		}
+    protected GenericUDAFEvaluator createEvaluator() {
+        return new GenericUDAFOrderedNValueEvaluator();
+    }
 
-		void init() {
-			sortKey = null;
-			value = null;
-		}
+    /*
+     * Keeps track of the Top key-value pair in the group
+     */
+    private static class TopOrderedSet implements AggregationBuffer {
+        private Text   sortKey;
+        private Object value;
 
-		void checkAndSet(Text key, Object value) {
-			if(sortKey == null  || (key!=null && sortKey.compareTo(key)>0)) {
-				sortKey = key;
-				this.value = value; 
-			}
-		}
+        TopOrderedSet() {
+            init();
+        }
 
-		public Object getTopValue() {
-			return value;
-		}
-		
-		public Map<Text, Object> getTopRecordAsMap() {
-			Map<Text, Object> topRec = new HashMap<Text, Object>(1);
-			topRec.put(sortKey, value);
-			return topRec;
-		}
-	}
-	
-	public static class GenericUDAFOrderedNValueEvaluator extends
-			GenericUDAFEvaluator {
-		private ObjectInspector valueOI;
-		private ObjectInspector[] sortKeyOI;
-		private StandardMapObjectInspector mapOI;
-		private ObjectInspector writableValueOI;
-		
-		@Override
-		public ObjectInspector init(Mode m, ObjectInspector[] parameters)
-				throws HiveException {
-			super.init(m, parameters);
-			// In complete & partial1 mode, we'll receive value to be returned & sortKeys
-			if (m.equals(Mode.COMPLETE) || m.equals(Mode.PARTIAL1)) {
-				valueOI = parameters[0];
-				sortKeyOI = new PrimitiveObjectInspector[parameters.length-1];
-				
-				for(int num=1; num<parameters.length; num++) {
-					sortKeyOI[num-1] = (PrimitiveObjectInspector) parameters[num];
-				}
-				writableValueOI = ObjectInspectorUtils
-						.getWritableObjectInspector(valueOI);
-			} else { 
-				//In partial2 & final mode, 
-				//we'll receive the intermediate map, with the top key-value pair for that partial aggregation 
-				mapOI = (StandardMapObjectInspector) parameters[0];
-			}
-			//In partial mode, the intermediate map containing top key-value pair for the partial data is returned
-			if (m == Mode.PARTIAL1 || m == Mode.PARTIAL2) {
-				ObjectInspector mapOI = ObjectInspectorFactory.getStandardMapObjectInspector(
-						PrimitiveObjectInspectorFactory.writableStringObjectInspector, writableValueOI);
-				return ObjectInspectorUtils
-						.getWritableObjectInspector(mapOI);
-			} else if(m == Mode.FINAL) {
-				// In final mode, the aggregated top column value for the group is returned
-				ObjectInspector writableValueOI = ObjectInspectorUtils
-						.getWritableObjectInspector(mapOI.getMapValueObjectInspector());
-				return writableValueOI;
-			}
-			return writableValueOI; 
-		}
+        void init() {
+            sortKey = null;
+            value = null;
+        }
 
-		@Override
-		public AggregationBuffer getNewAggregationBuffer() throws HiveException {
-			return new TopOrderedSet();
-		}
+        void checkAndSet(Text key, Object value) {
+            if (sortKey == null || (key != null && sortKey.compareTo(key) > 0)) {
+                sortKey = key;
+                this.value = value;
+            }
+        }
 
-		@Override
-		public void reset(AggregationBuffer agg) throws HiveException {
-			((TopOrderedSet) agg).init();
-		}
-		
-		@Override
-		public void iterate(AggregationBuffer agg, Object[] parameters)
-				throws HiveException {
-			TopOrderedSet fb = (TopOrderedSet) agg;
-			Object value = ObjectInspectorUtils.copyToStandardObject(parameters[0],
-					valueOI, ObjectInspectorCopyOption.WRITABLE);
-			StringBuilder sortKey = new StringBuilder();
-			for(int num=0; num<sortKeyOI.length; num++) {
-				if(parameters[num+1]!=null) {
-					Object key = ObjectInspectorUtils.copyToStandardObject(parameters[num+1],
-							sortKeyOI[num], ObjectInspectorCopyOption.WRITABLE);
-					sortKey.append(key);
-				}
-			}
-			// if the new sortKey is less than the current sortKey, set the new one
-			fb.checkAndSet(new Text(sortKey.toString()), value);
-		}
+        public Object getTopValue() {
+            return value;
+        }
 
-		@Override
-		public Object terminatePartial(AggregationBuffer agg)
-				throws HiveException {
-			Object map = ((TopOrderedSet) agg).getTopRecordAsMap();
-			return map;
-		}
-		
-		@Override
-		public void merge(AggregationBuffer agg, Object partial)
-				throws HiveException {
-			if (partial != null) {
-				TopOrderedSet myagg = (TopOrderedSet) agg;
-				Map<Object, Object> partialData = ((LazyBinaryMap) partial).getMap();
-				for(Map.Entry<Object, Object> data: partialData.entrySet()) {
-					myagg.checkAndSet((Text) data.getKey(), data.getValue());
-				}
-		     }
-		}
+        public Map<Text, Object> getTopRecordAsMap() {
+            Map<Text, Object> topRec = new HashMap<Text, Object>(1);
+            topRec.put(sortKey, value);
+            return topRec;
+        }
+    }
 
-		@Override
-		public Object terminate(AggregationBuffer agg) throws HiveException {
-			return ((TopOrderedSet) agg).getTopValue();
-		}
-	}
+    public static class GenericUDAFOrderedNValueEvaluator extends GenericUDAFEvaluator {
+        private ObjectInspector            valueOI;
+        private ObjectInspector[]          sortKeyOI;
+        private StandardMapObjectInspector mapOI;
+        private ObjectInspector            writableValueOI;
+
+        @Override
+        public ObjectInspector init(Mode m, ObjectInspector[] parameters) throws HiveException {
+            super.init(m, parameters);
+            // In complete & partial1 mode, we'll receive value to be returned &
+            // sortKeys
+            if (m.equals(Mode.COMPLETE) || m.equals(Mode.PARTIAL1)) {
+                valueOI = parameters[0];
+                sortKeyOI = new PrimitiveObjectInspector[parameters.length - 1];
+
+                for (int num = 1; num < parameters.length; num++) {
+                    sortKeyOI[num - 1] = (PrimitiveObjectInspector) parameters[num];
+                }
+                writableValueOI = ObjectInspectorUtils.getWritableObjectInspector(valueOI);
+            } else {
+                // In partial2 & final mode,
+                // we'll receive the intermediate map, with the top key-value
+                // pair for that partial aggregation
+                mapOI = (StandardMapObjectInspector) parameters[0];
+            }
+            // In partial mode, the intermediate map containing top key-value
+            // pair for the partial data is returned
+            if (m == Mode.PARTIAL1 || m == Mode.PARTIAL2) {
+                ObjectInspector mapOI = ObjectInspectorFactory.getStandardMapObjectInspector(PrimitiveObjectInspectorFactory.writableStringObjectInspector, writableValueOI);
+                return ObjectInspectorUtils.getWritableObjectInspector(mapOI);
+            } else if (m == Mode.FINAL) {
+                // In final mode, the aggregated top column value for the group
+                // is returned
+                ObjectInspector writableValueOI = ObjectInspectorUtils.getWritableObjectInspector(mapOI.getMapValueObjectInspector());
+                return writableValueOI;
+            }
+            return writableValueOI;
+        }
+
+        @Override
+        public AggregationBuffer getNewAggregationBuffer() throws HiveException {
+            return new TopOrderedSet();
+        }
+
+        @Override
+        public void reset(AggregationBuffer agg) throws HiveException {
+            ((TopOrderedSet) agg).init();
+        }
+
+        @Override
+        public void iterate(AggregationBuffer agg, Object[] parameters) throws HiveException {
+            TopOrderedSet fb = (TopOrderedSet) agg;
+            Object value = ObjectInspectorUtils.copyToStandardObject(parameters[0], valueOI, ObjectInspectorCopyOption.WRITABLE);
+            StringBuilder sortKey = new StringBuilder();
+            for (int num = 0; num < sortKeyOI.length; num++) {
+                if (parameters[num + 1] != null) {
+                    Object key = ObjectInspectorUtils.copyToStandardObject(parameters[num + 1], sortKeyOI[num], ObjectInspectorCopyOption.WRITABLE);
+                    sortKey.append(key);
+                }
+            }
+            // if the new sortKey is less than the current sortKey, set the new
+            // one
+            fb.checkAndSet(new Text(sortKey.toString()), value);
+        }
+
+        @Override
+        public Object terminatePartial(AggregationBuffer agg) throws HiveException {
+            Object map = ((TopOrderedSet) agg).getTopRecordAsMap();
+            return map;
+        }
+
+        @Override
+        public void merge(AggregationBuffer agg, Object partial) throws HiveException {
+            if (partial != null) {
+                TopOrderedSet myagg = (TopOrderedSet) agg;
+                Map<Object, Object> partialData = ((LazyBinaryMap) partial).getMap();
+                for (Map.Entry<Object, Object> data : partialData.entrySet()) {
+                    myagg.checkAndSet((Text) data.getKey(), data.getValue());
+                }
+            }
+        }
+
+        @Override
+        public Object terminate(AggregationBuffer agg) throws HiveException {
+            return ((TopOrderedSet) agg).getTopValue();
+        }
+    }
 }
