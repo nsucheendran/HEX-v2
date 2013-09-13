8010e4a85cccda2109aa70a8697c36a02ddf1819
Aditya Varun Chadha <adichad@gmail.com>
2013-09-13 15:59:23 +0530

    Added more log statements

--- src/main/scripts/LNX-HCOM_HEX_FIRST_ASSIGNMENT_HIT.sh
+++ src/main/scripts/LNX-HCOM_HEX_FIRST_ASSIGNMENT_HIT.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
-# load_first_assignment_hit.sh
+# LNX-HCOM_HEX_FIRST_ASSIGNMENT_HIT.sh
 # Wrapper to load first assignment hit data for HEX
 #
 # Usage:
-#  load_first_assignment_hit.sh
+#  LNX-HCOM_HEX_FIRST_ASSIGNMENT_HIT.sh
 #
 # Error codes:
 #   0 Success
@@ -26,7 +26,7 @@ source $PLAT_HOME/common/sh_helpers.sh
 source $PLAT_HOME/common/sh_metadata_storage.sh
 
 HWW_FAH_LOCK_NAME="hdp_first_assignment_hit.lock"
-MESSAGE="load_first_assignment_hit.sh failed: Previous script still running"
+MESSAGE="LNX-HCOM_HEX_FIRST_ASSIGNMENT_HIT.sh failed: Previous script still running"
 _ACQUIRE_LOCK $HWW_FAH_LOCK_NAME "$MESSAGE" 30
 
 STAGE_NAME="R1: HEX First Assignment Hit"
@@ -41,15 +41,15 @@ PROCESS_ID=$(_GET_PROCESS_ID "$PROCESS_NAME");
 RETURN_CODE="$?"
 
 if [ "$PROCESS_ID" == "" ] || (( $RETURN_CODE != 0 )); then
-    _LOG "ERROR: Process [$PROCESS_NAME] does not exist in HEMS"
-    ERROR_CODE=1
-	_FREE_LOCK $HWW_FAH_LOCK_NAME
-    exit 1;
+  _LOG "ERROR: Process [$PROCESS_NAME] does not exist in HEMS"
+  ERROR_CODE=1
+  _FREE_LOCK $HWW_FAH_LOCK_NAME
+  exit 1;
 else
-    RUN_ID=$(_RUN_PROCESS $PROCESS_ID "$PROCESS_NAME")
-    _LOG "PROCESS_ID=[$PROCESS_ID]"
-    _LOG "RUN_ID=[$RUN_ID]"
-    _LOG_PROCESS_DETAIL $RUN_ID "Started" "$ERROR_CODE"
+  RUN_ID=$(_RUN_PROCESS $PROCESS_ID "$PROCESS_NAME")
+  _LOG "PROCESS_ID=[$PROCESS_ID]"
+  _LOG "RUN_ID=[$RUN_ID]"
+  _LOG_PROCESS_DETAIL $RUN_ID "Started" "$ERROR_CODE"
 fi
 
 FAH_TABLE=`_READ_PROCESS_CONTEXT $PROCESS_ID "FAH_TABLE"`
@@ -57,6 +57,10 @@ FAH_DB=`_READ_PROCESS_CONTEXT $PROCESS_ID "FAH_DB"`
 JOB_QUEUE=`_READ_PROCESS_CONTEXT $PROCESS_ID "JOB_QUEUE"`
 LAST_DT=`_READ_PROCESS_CONTEXT $PROCESS_ID "BOOKMARK"`
 PROCESSING_TYPE=`_READ_PROCESS_CONTEXT $PROCESS_ID "PROCESSING_TYPE"`
+
+_LOG "PROCESSING_TYPE=$PROCESSING_TYPE"
+_LOG "LAST_DT=$LAST_DT"
+
 if [ $PROCESSING_TYPE = "R" ];
 then
   START_YEAR=`_READ_PROCESS_CONTEXT $PROCESS_ID "REPROCESS_START_YEAR"`
@@ -65,13 +69,21 @@ then
   END_YEAR=`date --date="${LAST_DT}" '+%Y'`
   END_MONTH=`date --date="${LAST_DT}" '+%m'`
 
+  _LOG "Starting Reprocessing for period: $START_YEAR-$START_MONTH to $END_YEAR-$END_MONTH (BOOKMARK=[$LAST_DT])"
+
   # drop monthly partitions that need to be reprocessed. reprocessing can only be specified at a year-month grain.
   CURR_YEAR=$START_YEAR
   CURR_MONTH=$START_MONTH
   while [ "${CURR_YEAR}${CURR_MONTH}" \< "${END_YEAR}${END_MONTH}" -o "${CURR_YEAR}${CURR_MONTH}" = "${END_YEAR}${END_MONTH}" ]
   do
     LOG_FILE_NAME="hdp_first_assignment_hit_reprocess_${CURR_YEAR}-${CURR_MONTH}.log"
-    time hive -hiveconf part.year="${CURR_YEAR}" -hiveconf part.month="${CURR_MONTH}" -hiveconf job.queue="${JOB_QUEUE}" -hiveconf hex.fah.db="${FAH_DB}" -hiveconf hex.fah.table="${FAH_TABLE}" -f $SCRIPT_PATH/delete_ETL_HEX_ASSIGNMENT_HIT.hql >> $HEX_LOGS/$LOG_FILE_NAME 2>&1
+    
+    _LOG "Dropping partition [$CURR_YEAR-$CURR_MONTH] from target: $FAH_DB.$FAH_TABLE"
+    hive -hiveconf part.year="${CURR_YEAR}" -hiveconf part.month="${CURR_MONTH}" -hiveconf job.queue="${JOB_QUEUE}" -hiveconf hex.fah.db="${FAH_DB}" -hiveconf hex.fah.table="${FAH_TABLE}" -f $SCRIPT_PATH/delete_ETL_HEX_ASSIGNMENT_HIT.hql >> $HEX_LOGS/$LOG_FILE_NAME 2>&1
+    ERROR_CODE=$?
+    if [[ $ERROR_CODE -ne 0 ]]; then
+      _LOG "ERROR while dropping partition [ERROR_CODE=$ERROR_CODE]"
+    fi
     NEW_YEAR=`date --date="${CURR_YEAR}-${CURR_MONTH}-01 00 +1 months" '+%Y'`
     CURR_MONTH=`date --date="${CURR_YEAR}-${CURR_MONTH}-01 00 +1 months" '+%m'`
     CURR_YEAR=$NEW_YEAR
@@ -106,20 +118,34 @@ then
       END_DATE=${arr2[0]}
       END_HOUR=${arr2[1]}
       LOG_FILE_NAME="hdp_first_assignment_hit_reprocess_${START_DATE}:${START_HOUR}-${END_DATE}:${END_HOUR}.log"
-      time hive -hiveconf start.date="${START_DATE}" -hiveconf start.hour="${START_HOUR}" -hiveconf end.date="${END_DATE}" -hiveconf end.hour="${END_HOUR}" -hiveconf job.queue="${JOB_QUEUE}" -hiveconf hex.fah.db="${FAH_DB}" -hiveconf hex.fah.table="${FAH_TABLE}" -hiveconf hex.lib="${HEX_LIB}" -hiveconf hex.version="${HEX_VERSION}" -f $SCRIPT_PATH/insert_ETL_HEX_ASSIGNMENT_HIT.hql >> $HEX_LOGS/$LOG_FILE_NAME 2>&1 
+
+      _LOG "Reprocessing First Assignment Hit data between [$START_DATE-START_HOUR to $END_DATE:$END_HOUR] from target: $FAH_DB.$FAH_TABLE"
+      hive -hiveconf start.date="${START_DATE}" -hiveconf start.hour="${START_HOUR}" -hiveconf end.date="${END_DATE}" -hiveconf end.hour="${END_HOUR}" -hiveconf job.queue="${JOB_QUEUE}" -hiveconf hex.fah.db="${FAH_DB}" -hiveconf hex.fah.table="${FAH_TABLE}" -hiveconf hex.lib="${HEX_LIB}" -hiveconf hex.version="${HEX_VERSION}" -f $SCRIPT_PATH/insert_ETL_HEX_ASSIGNMENT_HIT.hql >> $HEX_LOGS/$LOG_FILE_NAME 2>&1 
+      ERROR_CODE=$?
+      if [[ $ERROR_CODE -ne 0 ]]; then
+        _LOG "First Assignment Hit load FAILED [ERROR_CODE=$ERROR_CODE]"
+        _END_PROCESS $RUN_ID $ERROR_CODE
+        _FREE_LOCK $HWW_FAH_LOCK_NAME
+        exit 1
+      fi
     fi
 
     NEW_YEAR=`date --date="${CURR_YEAR}-${CURR_MONTH}-01 00 +1 months" '+%Y'`
     CURR_MONTH=`date --date="${CURR_YEAR}-${CURR_MONTH}-01 00 +1 months" '+%m'`
     CURR_YEAR=$NEW_YEAR
   done
+  _LOG "Done Reprocessing"
   if [ -z $LAST_DT ]; then
+    _LOG "Updating BOOKMARK (since none existed) as $END_DATE $END_HOUR"
     `_WRITE_PROCESS_CONTEXT "$PROCESS_ID" "BOOKMARK" "$END_DATE $END_HOUR"`
   fi
+  _LOG "Setting PROCESSING_TYPE to [D] for next run"
   `_WRITE_PROCESS_CONTEXT "$PROCESS_ID" "PROCESSING_TYPE" "D"`
   
 else
   # daily incremental load: uses list files to determine max contiguous delta upto 24 hrs available at source from the bookmark date
+
+  _LOG "Incremental First Assignment Hit data load (BOOKMARK=[$LAST_DT])"
   START_DT=`date --date="${LAST_DT} +1 hours" '+%Y-%m-%d:%H'`
   CURR_DATE=$LAST_DT
   END_DT=''
@@ -133,6 +159,13 @@ else
       break
     fi;
   done
+  ERROR_CODE=$?
+  if [[ $ERROR_CODE -ne 0 ]]; then
+    _LOG "First Assignment Hit load FAILED [ERROR_CODE=$ERROR_CODE]"
+    _END_PROCESS $RUN_ID $ERROR_CODE
+    _FREE_LOCK $HWW_FAH_LOCK_NAME
+    exit 1
+  fi
   if [ $END_DT ]; then
     OIFS=$IFS
     IFS=':'
@@ -146,10 +179,21 @@ else
     END_DATE=${arr2[0]}
     END_HOUR=${arr2[1]}
     LOG_FILE_NAME="hdp_first_assignment_hit_${START_DATE}:${START_HOUR}-${END_DATE}:${END_HOUR}.log"
-    time hive -hiveconf start.date="${START_DATE}" -hiveconf start.hour="${START_HOUR}" -hiveconf end.date="${END_DATE}" -hiveconf end.hour="${END_HOUR}" -hiveconf job.queue="${JOB_QUEUE}" -hiveconf hex.fah.db="${FAH_DB}" -hiveconf hex.fah.table="${FAH_TABLE}" -hiveconf hex.lib="${HEX_LIB}" -hiveconf hex.version="${HEX_VERSION}" -f $SCRIPT_PATH/insert_ETL_HEX_ASSIGNMENT_HIT.hql >> $HEX_LOGS/$LOG_FILE_NAME 2>&1 && _WRITE_PROCESS_CONTEXT "$PROCESS_ID" "BOOKMARK" "$END_DATE $END_HOUR" 
+    _LOG "Running First Assignment Hit incremental load for period: [$START_DATE:$START_HOUR to $END_DATE:$END_HOUR (BOOKMARK=[$LAST_DT])"
+    hive -hiveconf start.date="${START_DATE}" -hiveconf start.hour="${START_HOUR}" -hiveconf end.date="${END_DATE}" -hiveconf end.hour="${END_HOUR}" -hiveconf job.queue="${JOB_QUEUE}" -hiveconf hex.fah.db="${FAH_DB}" -hiveconf hex.fah.table="${FAH_TABLE}" -hiveconf hex.lib="${HEX_LIB}" -hiveconf hex.version="${HEX_VERSION}" -f $SCRIPT_PATH/insert_ETL_HEX_ASSIGNMENT_HIT.hql >> $HEX_LOGS/$LOG_FILE_NAME 2>&1 && _WRITE_PROCESS_CONTEXT "$PROCESS_ID" "BOOKMARK" "$END_DATE $END_HOUR" 
+    ERROR_CODE=$?
+    if [[ $ERROR_CODE -ne 0 ]]; then
+      _LOG "First Assignment Hit load FAILED [ERROR_CODE=$ERROR_CODE]"
+      _END_PROCESS $RUN_ID $ERROR_CODE
+      _FREE_LOCK $HWW_FAH_LOCK_NAME
+      exit 1
+    fi
+    _LOG "First Assignment Hit incremental load done."
+  else
+    _LOG "Contiguous delta not found from BOOKMARK=[$LAST_DT]. Nothing to do."
   fi
 fi
 
-
+_END_PROCESS $RUN_ID $ERROR_CODE
 _FREE_LOCK $HWW_FAH_LOCK_NAME
 
