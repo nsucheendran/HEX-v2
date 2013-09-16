dac3a401b639fbb0ae9d2acd00f0dae80a19f776
Aditya Varun Chadha <adichad@gmail.com>
2013-09-17 00:37:40 +0530

    trying out path changes

--- src/main/install/install.sh
+++ src/main/install/install.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
-set -e
+###############################################################################
+#Copyright (C) 2013 Expedia, Inc. All rights reserved.
+#
+#Description:
+#  Installation script for HEX ETL.
+#
+#Change History:
+#  Date        Author         Description
+#  ----------  -------------- ------------------------------------
+#  2013-09-13  achadha        Created
+###############################################################################
+set -u
+set +e
 
 export PLAT_HOME=/usr/local/edw/platform
 source $PLAT_HOME/common/sh_metadata_storage.sh
 
 HWW_HOME=/usr/etl/HWW
-SCRIPT_PATH=$HWW_HOME/hdp_hww_hex_etl/hql/R1
+MODULE_NAME=hdp_hww_hex_etl
+MODULE_LN=$HWW_HOME/$MODULE_NAME
+export MODULE_PATH=$MODULE_LN
 
+CURR_PATH=`dirname $0`
+
+SCRIPT_PATH=$CURR_PATH/../scripts/hql/R1
+
+ETL_USER=hwwetl
 FAH_TABLE='ETL_HCOM_HEX_ASSIGNMENT_HIT'
 FAH_DB='etldata'
 JOB_QUEUE='hwwetl'
@@ -21,7 +40,7 @@ FAH_PROCESS_DESCRIPTION="Loads HEX First Assignment Hit Data"
 
 FAH_PROCESS_ID=$(_GET_PROCESS_ID "$FAH_PROCESS_NAME")
 if [ -z "$FAH_PROCESS_ID" ]; then
-    hive -hiveconf job.queue="${JOB_QUEUE}" -hiveconf hex.fah.db="${FAH_DB}" -hiveconf hex.fah.table="${FAH_TABLE}" -f $SCRIPT_PATH/createTable_ETL_HEX_ASSIGNMENT_HIT.hql
+    hive -hiveconf job.queue="${JOB_QUEUE}" -hiveconf hex.fah.db="${FAH_DB}" -hiveconf hex.fah.table="${FAH_TABLE}" -f $SCRIPT_PATH/createTable_ETL_HCOM_HEX_ASSIGNMENT_HIT.hql
     $PLAT_HOME/tools/metadata/add_process.sh "$FAH_PROCESS_NAME" "$FAH_PROCESS_DESCRIPTION"
     FAH_PROCESS_ID=$(_GET_PROCESS_ID "$FAH_PROCESS_NAME")
 else
@@ -34,6 +53,12 @@ fi
 `_WRITE_PROCESS_CONTEXT $FAH_PROCESS_ID "PROCESSING_TYPE" "R"`
 `_WRITE_PROCESS_CONTEXT $FAH_PROCESS_ID "REPROCESS_START_YEAR" "$REPROCESS_START_YEAR"`
 `_WRITE_PROCESS_CONTEXT $FAH_PROCESS_ID "REPROCESS_START_MONTH" "$REPROCESS_START_MONTH"`
+
+# recreate the symbolic link to the deployed code 
+if [[ -r $MODULE_LN ]]; then 
+  sudo -u $ETL_USER unlink $MODULE_LN 
+fi 
+sudo -u $ETL_USER ln -sf $MODULE_DIR $MODULE_LN 
 
 _LOG "Process $FAH_PROCESS_NAME configured successfully"
 
