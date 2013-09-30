b8244d6fe246d379bc20ff20e1bcf228eca66249
Aditya Varun Chadha <adichad@gmail.com>
2013-10-01 01:12:37 +0530

    removing unused files

--- src/main/scripts/tools/helper_4_LNX-HCOM_HEX_TRANSACTION_BKG.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-_LOG "R3: Booking Transactions load FAILED [ERROR_CODE=$ERROR_CODE]. [see $HEX_LOGS/$LOG_FILE_NAME] for more information."
-_END_PROCESS $RUN_ID $ERROR_CODE
-_FREE_LOCK $HWW_TRANS_BKG_LOCK_NAME
