9b61e05a606375cdd76aa0540b440d05ad6bcd8b
Aditya Varun Chadha <adichad@gmail.com>
2013-12-02 23:27:01 +0530

    renaming script

--- src/main/scripts/LNX-HCOM_HEX_FACT.sh
+++ src/main/scripts/LNX-HCOM_HEX_FACT.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
-# LNX-HCOM_HEX_FACT_STAGE.sh
-# Wrapper to load hex fact staging data for HEX
+# LNX-HCOM_HEX_FACT.sh
+# Wrapper to load hex fact staging and final fact data for HEX
 #
 # Usage:
-#  LNX-HCOM_HEX_FACT_STAGE.sh
+#  LNX-HCOM_HEX_FACT.sh
 #
 # Error codes:
 #   0 Success
@@ -12,25 +12,26 @@
 #
 # user          date            comment
 # ############# ############### ####################
-# achadha       2013-10-21      Wrapper to incrementally load/reprocess Fact Staging 
+# achadha       2013-10-21      Wrapper to incrementally load/reprocess Fact Staging and Fact data 
 
 set -m
 export PLAT_HOME=/usr/local/edw/platform
 export HWW_HOME=/usr/etl/HWW
-SCRIPT_PATH=$HWW_HOME/hdp_hww_hex_etl/hql/FACT_STAGE
+SCRIPT_PATH=$HWW_HOME/hdp_hww_hex_etl/hql/FACT
+SCRIPT_PATH_REP=$HWW_HOME/hdp_hww_hex_etl/hql/REP
 HEX_LOGS=/usr/etl/HWW/log
 
 source $PLAT_HOME/common/sh_helpers.sh
 source $PLAT_HOME/common/sh_metadata_storage.sh
 
 HWW_LOCK_NAME="hdp_hww_hex_fact.lock"
-MESSAGE="LNX-HCOM_HEX_FACT_STAGE.sh failed: Previous script still running"
+MESSAGE="LNX-HCOM_HEX_FACT.sh failed: Previous script still running"
 _ACQUIRE_LOCK $HWW_LOCK_NAME "$MESSAGE" 30
 
-STAGE_NAME="HEX_FACT_STAGE: HEX Booking Fact Staging"
+STAGE_NAME="HEX_FACT_STAGE: HEX Fact Staging"
 _LOG_START_STAGE "$STAGE_NAME"
 
-PROCESS_NAME="ETL_HCOM_HEX_FACT_STAGE"
+PROCESS_NAME="ETL_HCOM_HEX_FACT"
 _LOG "PROCESS_NAME=[$PROCESS_NAME]"
 
 ERROR_CODE=0
@@ -50,14 +51,49 @@ else
   _LOG_PROCESS_DETAIL $RUN_ID "Started" "$ERROR_CODE"
 fi
 
-TABLE=`_READ_PROCESS_CONTEXT $PROCESS_ID "FACT_STAGE_TABLE"`
-DB=`_READ_PROCESS_CONTEXT $PROCESS_ID "FACT_STAGE_DB"`
+STAGE_TABLE=`_READ_PROCESS_CONTEXT $PROCESS_ID "FACT_STAGE_TABLE"`
+ACTIVE_FAH_TABLE=`_READ_PROCESS_CONTEXT $PROCESS_ID "ACTIVE_FAH_TABLE"`
+REPORT_TABLE=`_READ_PROCESS_CONTEXT $PROCESS_ID "REPORT_TABLE"`
+REPORT_FILE=`_READ_PROCESS_CONTEXT $PROCESS_ID "REPORT_FILE"`
+STAGE_DB=`_READ_PROCESS_CONTEXT $PROCESS_ID "STAGE_DB"`
 JOB_QUEUE=`_READ_PROCESS_CONTEXT $PROCESS_ID "JOB_QUEUE"`
-LAST_DT=`_READ_PROCESS_CONTEXT $PROCESS_ID "BOOKMARK"`
+SRC_BOOKMARK_OMNI=`_READ_PROCESS_CONTEXT $PROCESS_ID "SRC_BOOKMARK_OMNI"`
+SRC_BOOKMARK_BKG=`_READ_PROCESS_CONTEXT $PROCESS_ID "SRC_BOOKMARK_BKG"`
 PROCESSING_TYPE=`_READ_PROCESS_CONTEXT $PROCESS_ID "PROCESSING_TYPE"`
+
+if [ "${SRC_BOOKMARK_OMNI}" \< "${SRC_BOOKMARK_BKG}" ]
+then
+  MIN_SRC_BOOKMARK=$SRC_BOOKMARK_OMNI
+else
+  MIN_SRC_BOOKMARK=$SRC_BOOKMARK_BKG
+fi
 
 _LOG "PROCESSING_TYPE=$PROCESSING_TYPE"
 _LOG "LAST_DT=$LAST_DT"
+
+
+LOG_FILE_NAME="hdp_hex_fact_populate_reporting_table_${SRC_BOOKMARK_OMNI}-${SRC_BOOKMARK_BKG}.log"
+_LOG "loading reporting requirements table $REPORT_TABLE"
+hive -hiveconf hex.report.file="${REPORT_FILE}" -hiveconf mapred.job.queue.name="${JOB_QUEUE}" -hiveconf hex.db="${STAGE_DB}" -hiveconf hex.report.table="${REPORT_TABLE}" -f $SCRIPT_PATH_REP/createTable_HEX_REPORTING_REQUIREMENTS.hql >> $HEX_LOGS/$LOG_FILE_NAME 2>&1
+ERROR_CODE=$?
+if [[ $ERROR_CODE -ne 0 ]]; then
+  _LOG "HEX_FACT_STAGE: Reporting table load FAILED [ERROR_CODE=$ERROR_CODE]. See [$HEX_LOGS/$LOG_FILE_NAME] for more information."
+  _END_PROCESS $RUN_ID $ERROR_CODE
+  _FREE_LOCK $HWW_LOCK_NAME
+  exit 1
+fi
+
+hive -hiveconf min_src_bookmark="${MIN_SRC_BOOKMARK}" -hiveconf mapred.job.queue.name="${JOB_QUEUE}" -hiveconf hex.db="${STAGE_DB}" -hiveconf hex.report.table="${REPORT_TABLE}" -f $SCRIPT_PATH_REP/insert_HEX_REPORTING_REQUIREMENTS.hql >> $HEX_LOGS/$LOG_FILE_NAME 2>&1
+ERROR_CODE=$?
+if [[ $ERROR_CODE -ne 0 ]]; then
+  _LOG "HEX_FACT_STAGE: Reporting table load FAILED [ERROR_CODE=$ERROR_CODE]. See [$HEX_LOGS/$LOG_FILE_NAME] for more information."
+  _END_PROCESS $RUN_ID $ERROR_CODE
+  _FREE_LOCK $HWW_LOCK_NAME
+  exit 1
+fi
+_LOG "Done loading reporting requirements table $REPORT_TABLE"
+
+exit 0;
 
 if [ $PROCESSING_TYPE = "R" ];
 then
@@ -129,11 +165,11 @@ then
   _WRITE_PROCESS_CONTEXT "$PROCESS_ID" "PROCESSING_TYPE" "D"
 else
   # daily incremental load
-  _LOG "Incremental Booking Fact Staging data load (BOOKMARK=[$LAST_DT])"
-  START_DT=`date --date="${LAST_DT} +1 days" '+%Y-%m-%d'`
-  END_DT=$START_DT
+  _LOG "Incremental Booking Fact Staging data load (SRC_BOOKMARK_OMNI=[$SRC_BOOKMARK_OMNI], SRC_BOOKMARK_BKG=[$SRC_BOOKMARK_BKG])"
+  #START_DT=`date --date="${LAST_DT} +1 days" '+%Y-%m-%d'`
+  #END_DT=$START_DT
 
-  LOG_FILE_NAME="hdp_hex_fact_incremental_${START_DT}.log"
+  LOG_FILE_NAME="hdp_hex_fact_stage_incremental_${SRC_BOOKMARK_OMNI}-${SRC_BOOKMARK_BKG}.log"
   MONTH=`date --date="${START_DT}" '+%Y-%m'`
 
   hive -hiveconf into.overwrite="into" -hiveconf month="${MONTH}" -hiveconf start.date="${START_DT}" -hiveconf end.date="${END_DT}" -hiveconf job.queue="${JOB_QUEUE}" -hiveconf hex.db="${DB}" -hiveconf hex.table="${TABLE}" -f $SCRIPT_PATH/insert_ETL_HCOM_HEX_FACT_STAGE.hql >> $HEX_LOGS/$LOG_FILE_NAME 2>&1 
