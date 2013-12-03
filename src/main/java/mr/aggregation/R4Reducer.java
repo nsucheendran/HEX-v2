133d1a67a82a109bb1a337d31e06bd1e56d6c9f9
nsood <nsood@expedia.com>
2013-12-03 17:41:31 +0530

    R4 MR - Create sub-partitions [WIP]

--- src/main/java/mr/aggregation/R4Reducer.java
+++ src/main/java/mr/aggregation/R4Reducer.java
@@ -8,11 +8,28 @@ import mr.dto.TextMultiple;
 import mr.dto.UserTransactionData;
 import mr.dto.UserTransactionsAggregatedData;
 
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
 
 public class R4Reducer extends Reducer<TextMultiple, TextMultiple, NullWritable, TextMultiple> {
+    private MultipleOutputs mos;
+    private String outputDir;
+
+    public void setup(Context context) {
+        mos = new MultipleOutputs(context);
+        outputDir = context.getConfiguration().get("mapred.output.dir");
+    }
+
+    private String generateFileName(Text experimentCode, Text variantCode, Text versionNum) {
+        Path vcPath = new Path(outputDir, new Path("variant_code=" + variantCode));
+        Path ecPath = new Path(vcPath, new Path("experiment_code=" + experimentCode));
+        Path vnPath = new Path(ecPath, new Path("version_number=" + versionNum));
+        Path finalPath = new Path(vnPath, new Path("result"));
+        return finalPath.toString();
+    }
 
     @Override
     public void reduce(TextMultiple key, Iterable<TextMultiple> values, Context context) throws IOException, InterruptedException {
@@ -66,12 +83,25 @@ public class R4Reducer extends Reducer<TextMultiple, TextMultiple, NullWritable,
             netRoomNights += userAggTransData.getValue().getNetRoomNights();
             netGrossProfit += userAggTransData.getValue().getNetGrossProfit();
         }
-        context.write(
+        System.out.println("key>>>>>" + key);
+        mos.write(
                 NullWritable.get(),
                 new TextMultiple(key, Long.toString(numUniqueViewers), Long.toString(numUniquePurchasers), Long
-                        .toString(numUniqueCancellers), Long.toString(numActivePurchasers), Long.toString(numNilNetOrdersPurchasers),
-                        Long.toString(numCancellations), Long.toString(netOrders), Double.toString(netGBV), Long.toString(netRoomNights),
+                        .toString(numUniqueCancellers), Long.toString(numActivePurchasers), Long.toString(numNilNetOrdersPurchasers), Long
+                        .toString(numCancellations), Long.toString(netOrders), Double.toString(netGBV), Long.toString(netRoomNights),
                 /* omnitureGBV and omnitureRoomNights? */
-                        Double.toString(netGrossProfit), Long.toString(numRepeatPurchasers)));
+                Double.toString(netGrossProfit), Long.toString(numRepeatPurchasers)),
+                generateFileName(key.getTextElementAt(2), key.getTextElementAt(3), key.getTextElementAt(4)));
+        /*
+         * context.write( NullWritable.get(), new TextMultiple(key, Long.toString(numUniqueViewers), Long.toString(numUniquePurchasers),
+         * Long .toString(numUniqueCancellers), Long.toString(numActivePurchasers), Long.toString(numNilNetOrdersPurchasers),
+         * Long.toString(numCancellations), Long.toString(netOrders), Double.toString(netGBV), Long.toString(netRoomNights),
+         * 
+         * Double.toString(netGrossProfit), Long.toString(numRepeatPurchasers)));
+         */
+    }
+    
+    public void cleanup(Context context) throws IOException, InterruptedException {
+        mos.close();
     }
 }
