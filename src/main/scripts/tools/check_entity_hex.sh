1ad7f2d389bf112ab334b5d2a805aba2e62807f4
Aditya Varun Chadha <adichad@gmail.com>
2014-01-14 03:21:57 +0530

    attempted fix for r3 email issue: no access to read HEMS for edw_ctmjob

--- src/main/scripts/tools/check_entity_hex.sh
+++ src/main/scripts/tools/check_entity_hex.sh
@@ -2,6 +2,7 @@
 
 
 export PLAT_HOME=/usr/local/edw/platform
+export HWW_HOME=/usr/etl/HWW
 
 source $PLAT_HOME/common/sh_helpers.sh
 source $PLAT_HOME/common/sh_metadata_storage.sh
@@ -23,11 +24,19 @@ else
   RUN_ID=$(_RUN_PROCESS $PROCESS_ID "$PROCESS_NAME")
   _LOG "PROCESS_ID=[$PROCESS_ID]"
   _LOG "RUN_ID=[$RUN_ID]"
-  _LOG_PROCESS_DETAIL $RUN_ID "Started" "$ERROR_CODE"
 fi
 
 BOOKMARK=`_READ_PROCESS_CONTEXT $PROCESS_ID "BOOKMARK"`
 PROCESSING_TYPE=`_READ_PROCESS_CONTEXT $PROCESS_ID "PROCESSING_TYPE"`
+EMAIL_TO=`_READ_PROCESS_CONTEXT $PROCESS_ID "EMAIL_TO"`
+EMAIL_CC=`_READ_PROCESS_CONTEXT $PROCESS_ID "EMAIL_CC"`
+
+EMAIL_RECIPIENTS=$EMAIL_TO
+if [ $EMAIL_CC ]
+then
+  EMAIL_RECIPIENTS="-c $EMAIL_CC $EMAIL_RECIPIENTS"
+fi
+
 
 _LOG "PROCESSING_TYPE=$PROCESSING_TYPE"
 _LOG "BOOKMARK=$BOOKMARK"
@@ -38,7 +47,6 @@ else
   export END_DATE=`date --date="${BOOKMARK} +1 days" '+%Y-%m-%d'`
 fi
 
-export HWW_HOME=/usr/etl/HWW
 source $HWW_HOME/common/sh_hww_helpers.sh
 
 export ENTITY=$1
@@ -56,5 +64,6 @@ if [[ $ERROR_CODE -eq 0 && $STATUS -eq 1 ]]; then
   exit 0
 else
   echo "[$ENTITY] has not completed"
+  echo -e "====================================================================================================================================================================\ncheck_entity_hex.sh failed: No new data available from BOOKMARK=[${BOOKMARK}] in source.\nCheck source data availability (Refer to documentation : https://confluence/pages/viewpage.action?pageId=420855780)\n\nScript Name : $0\n====================================================================================================================================================================" | mailx -s "HWW HEX Alert (ETL_HCOM_HEX_TRANSACTIONS_BKG): No incremental data in source to process (Last Bookmark Date - ${BOOKMARK})" $EMAIL_RECIPIENTS
   exit 1
 fi
--- src/main/scripts/tools/run_dbsync_hex.sh
+++ src/main/scripts/tools/run_dbsync_hex.sh
@@ -5,6 +5,7 @@ export ETL_USER=platetl
 export PLAT_HOME=/usr/local/edw/platform
 export HWW_HOME=/usr/etl/HWW
 SCRIPT_PATH=$HWW_HOME/hdp_hww_hex_etl
+PROCESS_NAME="ETL_HCOM_HEX_TRANSACTIONS_BKG"
 
 # DBSYNC variables
 export DS_NAME="EDW_DB2"
@@ -17,9 +18,42 @@ export HIVE_TABLE_DIR="/data/common/$HIVE_SCHEMA/$HIVE_TABLE_NAME"
 export TEMP_DIR="/tmp/common/$HIVE_SCHEMA/$HIVE_TABLE_NAME"
 export DB2_TABLE=$DB2_TABLE_NAME
 
+PROCESS_ID=$(_GET_PROCESS_ID "$PROCESS_NAME");
+RETURN_CODE="$?"
+
+if [ "$PROCESS_ID" == "" ] || (( $RETURN_CODE != 0 )); then
+  _LOG "ERROR: Process [$PROCESS_NAME] does not exist in HEMS"
+  ERROR_CODE=1
+  _FREE_LOCK $HWW_TRANS_BKG_LOCK_NAME
+  exit 1;
+else
+  RUN_ID=$(_RUN_PROCESS $PROCESS_ID "$PROCESS_NAME")
+  _LOG "PROCESS_ID=[$PROCESS_ID]"
+  _LOG "RUN_ID=[$RUN_ID]"
+fi
+
+EMAIL_TO=`_READ_PROCESS_CONTEXT $PROCESS_ID "EMAIL_TO"`
+EMAIL_CC=`_READ_PROCESS_CONTEXT $PROCESS_ID "EMAIL_CC"`
+
+EMAIL_RECIPIENTS=$EMAIL_TO
+if [ $EMAIL_CC ]
+then
+  EMAIL_RECIPIENTS="-c $EMAIL_CC $EMAIL_RECIPIENTS"
+fi
+
 cd /tmp
 source $PLAT_HOME/tools/sh_dbsync/dbsync_helpers.sh
-_SYNCHRONIZE_TABLE || exit 1
+_SYNCHRONIZE_TABLE
+ERROR_CODE=$?
+if [ $ERROR_CODE -ne 0 ]; then
+  echo -e "====================================================================================================================================================================\nrun_dbsync_hex.sh failed with ERROR_CODE=$ERROR_CODE: No new data available from BOOKMARK=[${BOOKMARK}] in source.\nCheck source data availability (Refer to documentation : https://confluence/pages/viewpage.action?pageId=420855780)\n\nScript Name : $0\n====================================================================================================================================================================" | mailx -s "HWW HEX Alert (ETL_HCOM_HEX_TRANSACTIONS_BKG) ERROR: failed to load incremental data from source (Last Bookmark Date - ${BOOKMARK})" $EMAIL_RECIPIENTS
+  exit 1
+fi;
 
 export DS_NAME="EDW_DB2_CLI"
-$SCRIPT_PATH/tools/db2hive_counts_validation.sh || exit 1
+$SCRIPT_PATH/tools/db2hive_counts_validation.sh
+ERROR_CODE=$?
+if [ $ERROR_CODE -ne 0 ]; then
+  echo -e "====================================================================================================================================================================\nrun_dbsync_hex.sh failed with ERROR_CODE=$ERROR_CODE: No new data available from BOOKMARK=[${BOOKMARK}] in source.\nCheck source data availability (Refer to documentation : https://confluence/pages/viewpage.action?pageId=420855780)\n\nScript Name : $0\n====================================================================================================================================================================" | mailx -s "HWW HEX Alert (ETL_HCOM_HEX_TRANSACTIONS_BKG) ERROR: failed to load incremental data from source (Last Bookmark Date - ${BOOKMARK})" $EMAIL_RECIPIENTS
+  exit 1
+fi;
