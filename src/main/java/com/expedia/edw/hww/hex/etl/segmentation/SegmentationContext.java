88b67617829a4d45599472848fc560e4d4772c6b
Abhimanyu Gupta <abhimanyu.gupta07@gmail.com>
2014-06-18 16:26:55 +0530

    integrating segmentation job with jcommander

---
+++ src/main/java/com/expedia/edw/hww/hex/etl/segmentation/SegmentationContext.java
@@ -0,0 +1,25 @@
+package com.expedia.edw.hww.hex.etl.segmentation;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+import com.expedia.edw.hww.common.hadoop.metrics.StatsWriterFactoryBean;
+
+@Configuration
+@ComponentScan("com.expedia.edw.hww.hex.etl.segmentation")
+public class SegmentationContext {
+
+  @Bean
+  public static PropertySourcesPlaceholderConfigurer placeHolderConfigurer() {
+    PropertySourcesPlaceholderConfigurer placeholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+    placeholderConfigurer.setNullValue("null");
+    return placeholderConfigurer;
+  }
+
+  @Bean
+  public StatsWriterFactoryBean statsWriter() {
+    return new StatsWriterFactoryBean();
+  }
+}
